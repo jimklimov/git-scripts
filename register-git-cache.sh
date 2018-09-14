47f4869baf8701f4a12e98755921b27e3c0f77fa
Jim Klimov <jimklimov@gmail.com>
2018-09-14 16:59:48 +0200

    register-git-cache.sh : avoid failed "shift" for fetch without args

--- register-git-cache.sh
+++ register-git-cache.sh
@@ -167,11 +167,11 @@ EOF
             shift
             ;;
         fetch|update|pull|up)
-            shift
-            if [ "$#" = 0 ]; then
+            if [ "$#" = 1 ]; then
                 git fetch --all -P4 --prune 2>/dev/null || \
                 git fetch --all --prune
             else
+                shift
                 do_fetch_repos "$@" ; exit $?
             fi
             ;;
