daf8adb5c7619234d1b61fd0f0c692192850b5ed
Jim Klimov <jim@jimklimov.com>
2021-01-29 20:25:09 +0100

    register-git-cache.sh: do_list_subrepos(): also do not dig into processing if HASH.gitmodules-urls.tmp exists

--- register-git-cache.sh
+++ register-git-cache.sh
@@ -236,7 +236,7 @@ do_list_subrepos() {
         while read HASH REFREPODIR_REPO ; do
             (   # Note the 'test -e': here we assume that a file creation
                 # and population attempt was successful
-                if [ ! -e "${TEMPDIR_BASE}/${HASH}:.gitmodules-urls" ] ; then
+                if [ ! -e "${TEMPDIR_BASE}/${HASH}:.gitmodules-urls" ] && [ ! -e "${TEMPDIR_BASE}/${HASH}:.gitmodules-urls.tmp" ] ; then
                     [ -n "${REFREPODIR_REPO}" ] \
                     && { pushd "${REFREPODIR_BASE}/${REFREPODIR_REPO}" >/dev/null || exit $? ; }
                     ###echo "======= Checking submodules (if any) under tip hash '$HASH' '$REFREPODIR_REPO' '`pwd`'..." >&2
